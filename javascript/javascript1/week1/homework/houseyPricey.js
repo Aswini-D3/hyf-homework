94bfb32c63d06f1e8415975fcde9d8df13fe5fe6
Aswini-D3 <reddyaswini@gmail.com>
2020-03-07 10:16:56 +0100

    Changes done in houseyPricey.js

--- javascript/javascript1/week1/homework/houseyPricey.js
+++ javascript/javascript1/week1/homework/houseyPricey.js
@@ -1,11 +1,11 @@
 /*About Peter house */
 
-let peterHouseWide = 8;
-let peterHouseDeep = 10;
-let peterHouseHigh = 10;
-let peterGardenSize = 100;
-let peterHousevolumeInMeters = peterHouseWide * peterHouseDeep * peterHouseHigh;
-let peterhousePrice = peterHouseWide * 2.5 * 1000 + peterGardenSize * 300;
+const peterHouseWide = 8;
+const peterHouseDeep = 10;
+const peterHouseHigh = 10;
+const peterGardenSize = 100;
+const peterHousevolumeInMeters = peterHouseWide * peterHouseDeep * peterHouseHigh;
+const peterhousePrice = peterHouseWide * 2.5 * 1000 + peterGardenSize * 300;
 if (peterhousePrice>=2500000) {
     console.log("Peter is paying more");
     
@@ -16,12 +16,12 @@ if (peterhousePrice>=2500000) {
 
 /*About Julia house*/
 
-let juliaHouseWide = 5;
-let juliaHouseDeep = 11;
-let juliaHouseHigh = 8;
-let juliaGardenSize = 70;
-let juliaHousevolumeInMeters = peterHouseWide * peterHouseDeep * peterHouseHigh;
-let juliahousePrice = peterHouseWide * 2.5 * 1000 + peterGardenSize * 300;
+const juliaHouseWide = 5;
+const juliaHouseDeep = 11;
+const juliaHouseHigh = 8;
+const juliaGardenSize = 70;
+const juliaHousevolumeInMeters = peterHouseWide * peterHouseDeep * peterHouseHigh;
+const juliahousePrice = peterHouseWide * 2.5 * 1000 + peterGardenSize * 300;
 if (juliahousePrice>=2500000) {
     console.log("Julia is paying more");
     
