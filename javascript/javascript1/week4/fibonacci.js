0208a578334c17054efc863dd93b06990c94ca50
Aswini-D3 <reddyaswini@gmail.com>
2020-03-29 23:18:08 +0200

    added fibonacci

---
+++ javascript/javascript1/week4/fibonacci.js
@@ -0,0 +1,8 @@
+function fibonacci(n){
+    let result = [0, 1];
+    for (let i = 2; i < n + 1; i++){
+      result.push(result[i - 2] + result[i -1])
+    }
+   return result[n]
+  }
+  console.log(fibonacci(6));
