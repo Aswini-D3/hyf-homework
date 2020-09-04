789b6ffe69873455614c0f2f7f3d3392135b4f98
Aswini-D3 <reddyaswini@gmail.com>
2020-09-04 13:22:44 +0200

    added week5

---
+++ react/week5/github user search-week5/src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const About = () => (
+  <Fragment>
+    <h1>About</h1>
+    <p>An app to search Github users.</p>
+  </Fragment>
+);
+
+export default About;
